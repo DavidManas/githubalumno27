5fb5113f2c5f75aa5b9953931b9d0703938fa465
DavidManas <davidmanas95@gmail.com>
2023-03-08 20:01:23 +0000

    subidos archivos nombreJava27

--- nombreJava27.java
+++ nombreJava27.java
@@ -1,5 +1,6 @@
 public class nombreJava27 {
 	public static void main(String[] args) {
-		System.out.println("Me llamo David Mañas y he creado el servidor ubunserver27");
+		System.out.println("Me llamo David Mañas, he creado el servidor ubunserver27" );
+		System.out.println("y vivo en San Fernando de Henares");
 	}
 }
